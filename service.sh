9bc4c760add520313484cfacc17e0e9928f19a68
UsiFX <91332949+UsiFX@users.noreply.github.com>
2021-10-01 15:12:59 +0200

    add some important scripts to be updatable at every boot
    *+* Line 18 > 23  : add XNET, XPERF , module.prop, system.prop to be updatable , so no need to make entire beta tag for every one fix

--- service.sh
+++ service.sh
@@ -15,6 +15,13 @@ MODPATH=/data/adb/modules/Thermod.XADB
 TPATH=/sys/class/thermal/thermal_message/sconfig
 LOG=/storage/emulated/0/XCORE/TX.log
 
+# Update main scripts
+_wget -O "${MODPATH}/system/bin/XNET" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XNET"
+_wget -O "${MODPATH}/system/bin/XPERF" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XPERF"
+_wget -O "${MODPATH}/module.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/module.prop"
+_wget -O "${MODPATH}/system.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system.prop"
+_wget -O "${MODPATH}/ThermodXbanner" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/ThermodXbanner"
+
 Path=/sdcard
 if [ ! -d $Path/XCORE ]; then
  mkdir -p $Path/XCORE
