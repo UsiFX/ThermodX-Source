6653fb2f56ce2135696df442b7b034e23402039a
UsiFX <91332949+UsiFX@users.noreply.github.com>
2021-10-07 21:31:06 +0200

    use wget extension

--- service.sh
+++ service.sh
@@ -1,74 +1,66 @@
 #!/system/bin/sh
-# ThermodX LOGIC
+# ThermodX Logic
 
-_sleep_to_boot_complete() {
-until [[ "$(getprop sys.boot_completed)" == "1" ]] || [[ "$(getprop dev.bootcomplete)" == "1" ]]
-do
-       sleep 1
-done
+MODPATH="/data/adb/modules/ThermodX.ADB"
+TPATH="/sys/class/thermal/thermal_message/sconfig"
+LOG="/storage/emulated/0/XCORE/TX.log"
+
+# Wget Tool Function
+_wget() {
+    /data/adb/modules/xtweak/bin/wget "$@"
 }
 
 # Sleep until boot completed
-_sleep_to_boot_complete
+until ["$(getprop sys.boot_completed)" = "1"] || ["$(getprop dev.bootcomplete)" = "1"]
+do
+sleep 1
+done
 
-MODPATH=/data/adb/modules/Thermod.XADB
-TPATH=/sys/class/thermal/thermal_message/sconfig
-LOG=/storage/emulated/0/XCORE/TX.log
-
-# Update main scripts
-_wget -O "${MODPATH}/system/bin/XNET" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XNET"
-_wget -O "${MODPATH}/system/bin/XPERF" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XPERF"
-_wget -O "${MODPATH}/module.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/module.prop"
-_wget -O "${MODPATH}/system.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system.prop"
-_wget -O "${MODPATH}/ThermodXbanner" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/ThermodXbanner"
+# Sleep until some time to complete boot and init
+sleep 60
 
 Path=/sdcard
-if [ ! -d $Path/XCORE ]; then
- mkdir -p $Path/XCORE
+if [! -d $Path/XCORE]; then
+mkdir -p $Path/XCORE
 fi
 clear
 
 # clear log
 rm $LOG
 
-# wait for boot to finish first
-BOOT=$(getprop getprop sys.boot_completed)
-if [ "$BOOT" = "0" ];then
-sleep 3
-fi
+# Update main scripts
+_wget -O "$ {
+    MODPATH
+}/system/bin/XNET" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XNET"
+_wget -O "$ {
+    MODPATH
+}/system/bin/XPERF" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system/bin/XPERF"
+_wget -O "$ {
+    MODPATH
+}/module.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/module.prop"
+_wget -O "$ {
+    MODPATH
+}/system.prop" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/system.prop"
+_wget -O "$ {
+    MODPATH
+}/ThermodXbanner" "https://raw.githubusercontent.com/UsiFX/ThermodX-Source/main/ThermodXbanner"
 
-# Wait for boot to finish completely
-while [[ `getprop sys.boot_completed` -ne 1 ]] && [[ ! -d "/sdcard" ]]
-do
-       sleep 1
-done
-
-# Sleep an additional 3s to ensure init is finished
-sleep 3
+# Start qcom optimization
+sh "/system/bin/xqcom"
 
 # sync before exuecuting scripts
-sync 
+sync
 
-setup_tweaks() {
-if [ -f /sys/class/thermal/thermal_message/sconfig ]; then
+if [-f /sys/class/thermal/thermal_message/sconfig]; then
 echo 10 > /sys/class/thermal/thermal_message/sconfig
 fi
 
-if [ $(cat /sys/class/kgsl/kgsl-3d0/throttling) = 1 ]; then
+if [$(cat /sys/class/kgsl/kgsl-3d0/throttling) = 1]; then
 echo 0 > /sys/class/kgsl/kgsl-3d0/throttling
 fi
-elif [ -f $MODPATH/XPERF]; then
-sh $MODPATH/XPERF
-sh /system/bin/XPERF
+elif [-f $MODPATH/XPERF]; then
+sh "$MODPATH/XPERF"
 sleep 0.5
-elif [ -f $MODPATH/XNET ]; then
-sh $MODPATH/XNET
+elif [-f $MODPATH/XNET]; then
+sh "$MODPATH/XNET"
 fi
-}
-
-setup_tweaks
-
-# in some case , it will reset to default mode , so lock it
-chmod 444 $TPATH
-
-exit 0
