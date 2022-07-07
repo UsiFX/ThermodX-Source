09dc06998dd114d8e70c5658c7a91c76c7946217
UsiFX <xprjkts@gmail.com>
2022-07-07 03:41:19 +0200

    treewide : resourcing
    - Time to reborn...
    
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- zipper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# ThermodX Zipper Script
-# Author: UsiF (UsiFX @ github)
-# Credits: LOOPER (iamlooper @ github)
-echo "[*] Give me version name. "
-read name 
-zip -r9T "ThermodX_${name}.zip" . -x zipper.sh -x *.git*
-echo "[*] Done! "
