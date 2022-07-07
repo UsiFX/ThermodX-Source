569ecbb742199465bdf2803b05213148bb828ff1
UsiFX <xprjkts@gmail.com>
2022-07-07 07:44:59 +0200

    addon: resort chooseport()
    - code resorted to avoid syntax issues
    - this may fix installation if the bar have been looped
    - or something went wrong
    
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- addon/Volume-Key-Selector/install.sh
+++ addon/Volume-Key-Selector/install.sh
@@ -28,30 +28,35 @@ chooseport_legacy() {
 
 chooseport() {
   # Original idea by chainfire and ianmacd @xda-developers
-  [ "$1" ] && local delay=$1 || local delay=3
-  local error=false
-  while true; do
-    local count=0
-    while true; do
-      timeout $delay /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events &
-      sleep 0.5; count=$((count + 1))
-      if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
-        return 0
-      elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
-        return 1
-      fi
-      [ $count -gt 6 ] && break
-    done
-    if $error; then
-      echo "Volume key not detected. Trying keycheck method"
-      export chooseport=chooseport_legacy VKSEL=chooseport_legacy
-      chooseport_legacy $delay
-      return $?
-    else
-      error=true
-      echo "Volume key not detected. Try again"
-    fi
-  done
+	if [ $1 == * ]; then
+		set delay=$1
+	else
+		delay=3
+	fi
+	error=false
+  	while true; do
+    	count=0
+    		while true; do
+			timeout $delay /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events
+			sleep 0.5
+			count=$((count + 1))
+			if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
+				return 0
+			elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
+				return 1
+			fi
+    			[ $count -gt 6 ] && break
+		done
+		if $error; then
+			echo "Volume key not detected. Trying keycheck method"
+			export chooseport=chooseport_legacy VKSEL=chooseport_legacy
+			chooseport_legacy $delay
+			return $?
+		else
+      			error=true
+			echo "Volume key not detected. Try again"
+		fi
+	done
 }
 
 # Keep old variable from previous versions of this
